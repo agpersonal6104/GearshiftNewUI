2c5c82e998dc3be0bf3670e97ca7ebebd0618b75
AyushG <legendsteamprivate@gmail.com>
2025-02-25 13:46:15 +0530

    Preloader decided

--- src/components/PreLoader.jsx
+++ src/components/PreLoader.jsx
@@ -25,9 +25,9 @@ const PreLoader = () => {
         <h1 className='text-5xl italic font-bold text-center text-white'>Gearshift Garages</h1>
       </div>
        
-      <div className='lg:h-[80%] lg:flex flex-col gap-2 justify-end items-center py-12 h-[70%] hidden '>
+      <div className='lg:h-[80%] lg:flex flex-col gap-1 justify-end items-center py-20 h-[60%] hidden '>
         
-        <h1 className='text-5xl italic font-bold text-white'>Gearshift Garages</h1>
+        <h1 className='text-4xl italic font-bold text-white'>Gearshift Garages</h1>
         <TextTransition springConfig={presets.wobbly} className='text-2xl italic text-white'>{TEXTS[index % TEXTS.length]}</TextTransition>
         
       </div>
@@ -40,7 +40,7 @@ const PreLoader = () => {
         <h2 className='text-xl italic text-center text-white'>Your next Destination Starts Now..</h2>
       </div>
       
-      <div className='h-[15%] flex flex-col justify-center items-center'>
+      <div className='h-[12%] flex flex-col justify-center items-center'>
         
             <div className="progress-loader">
                 <div className="progress" />
